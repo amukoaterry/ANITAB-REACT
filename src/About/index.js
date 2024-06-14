812e041d5bf1232665f1f1bfb9ec16780358f84a
amukoaterry <amukoaterrymitchell@gmail>
2024-06-14 21:14:31 +0300

    first commit

---
+++ src/About/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+
+const About = () => {
+    return (
+        <div className="about-text">
+
+        <p>We are learning about js</p>
+        <p>We are coding in a new Front-end Library</p>
+
+        </div>
+    )
+}
+
+export default About;
